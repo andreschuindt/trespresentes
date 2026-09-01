c738bccea45e0eedfc7b8ea7a51ac28b61a1bc70
andreschuindt <andreschuindt@gmail.com>
2026-09-01 20:46:15 -0300

    Publica imagem oficial do Efeito Girassol

--- api/ebook-panel.js
+++ api/ebook-panel.js
@@ -1,15 +1,6 @@
-const parts = [
-  require('../data/ebook-panel-data-00'),
-  require('../data/ebook-panel-data-01'),
-  require('../data/ebook-panel-data-02'),
-  require('../data/ebook-panel-data-03'),
-  require('../data/ebook-panel-data-04')
-];
-
 module.exports = (req, res) => {
-  const data = Buffer.from(parts.join(''), 'base64');
-  res.setHeader('Content-Type', 'image/webp');
-  res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
-  res.statusCode = 200;
-  res.end(data);
+  res.statusCode = 302;
+  res.setHeader('Location', '/tres-presentes-ebook-oficial.png?v=20260901-1');
+  res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=31536000');
+  res.end();
 };
